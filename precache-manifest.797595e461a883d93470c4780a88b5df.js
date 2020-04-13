92b0c23181c93b1c53f5487183d88180bd831ab3
Mahmoud Abdo <mahmoud.abdo59986@gmail.com>
2020-04-13 04:43:48 +0200

    Updates

---
+++ precache-manifest.797595e461a883d93470c4780a88b5df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84a0ab66ca49561b72ca5ead369a8341",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "502e682b998400e0c97c",
+    "url": "/monsters-rolodex/static/css/main.ed70eec6.chunk.css"
+  },
+  {
+    "revision": "d22909dcec216c275287",
+    "url": "/monsters-rolodex/static/js/2.57440af1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.57440af1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "502e682b998400e0c97c",
+    "url": "/monsters-rolodex/static/js/main.a728444a.chunk.js"
+  },
+  {
+    "revision": "7303831af92bcb985586",
+    "url": "/monsters-rolodex/static/js/runtime-main.1f898dd8.js"
+  }
+]);
